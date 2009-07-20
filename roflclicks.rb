bf602a41ec8294cdf8ebf63f20a2557b659259d2
Zach Holman <github.com@zachholman.com>
2009-07-20 02:57:03 -0700

    This shit is going to be funny.

--- roflclicks.rb
+++ roflclicks.rb
@@ -1,6 +1,26 @@
 require 'rubygems'
 require 'sinatra'
+require 'haml'
+require 'sqlite3'
+
+helpers do
+  def number_with_delimiter(number, *args)
+    delimiter ||= ','
+    separator ||= ','
+  
+    begin
+      parts = number.to_s.split('.')
+      parts[0].gsub!(/(\d)(?=(\d\d\d)+(?!\d))/, "\\1#{delimiter}")
+      parts.join(separator)
+    rescue
+      number
+    end
+  end
+end
 
 get '/' do
-  "Hello from Sinatra on Heroku!"
+  db = SQLite3::Database.new("roflclicks.db")
+  db.execute("update clicks set total = total+1")
+  @total = number_with_delimiter db.get_first_row("select total from clicks").first.to_i
+  haml "index_#{(1..3).to_a[rand(3)]}".to_sym
 end
