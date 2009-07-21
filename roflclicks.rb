aad7956a457908f45fa154f4941a3bacd5fc1776
Zach Holman <github.com@zachholman.com>
2009-07-21 01:55:56 -0700

    updating some datamapper config

--- roflclicks.rb
+++ roflclicks.rb
@@ -1,9 +1,20 @@
 require 'rubygems'
 require 'sinatra'
 require 'haml'
-require 'sqlite3'
+require 'dm-core'
 
 @@total_views = Dir.entries("views").size - 3
+
+class Click
+  include DataMapper::Resource
+  property :total, Integer
+end
+ 
+configure do
+  DataMapper.setup(:default, (ENV["DATABASE_URL"] || "sqlite3:///#{Dir.pwd}/roflclicks.sqlite3"))
+  DataMapper.auto_upgrade!
+end
+
 
 helpers do
   def number_with_delimiter(number, *args)
@@ -21,8 +32,8 @@ helpers do
 end
 
 get '/' do
-  db = SQLite3::Database.new("roflclicks.sqlite3")
-  db.execute("update clicks set total = total+1")
-  @total = number_with_delimiter db.get_first_row("select total from clicks").first.to_i
+  click = Click.first
+  click.update_attributes(:total => click.total+1)
+  @total = number_with_delimiter click.total.to_i
   haml "index_#{(1..@@total_views).to_a[rand(@@total_views)]}".to_sym
 end
